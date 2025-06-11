032c0d48f0fbae7cdb37649b4bb423940a93ca88
San Kang <>
2025-06-11 18:11:37 +0900

    feat : spring boot template

---
+++ src/test/java/com/san/newsfeed/NewsfeedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.san.newsfeed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsfeedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
