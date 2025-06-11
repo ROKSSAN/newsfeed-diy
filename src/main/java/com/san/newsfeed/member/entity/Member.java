8524e540b8ac648187d8130f21ec0ea7fcdeed8e
San Kang <>
2025-06-11 19:45:26 +0900

    feat : spring boot template

---
+++ src/main/java/com/san/newsfeed/member/entity/Member.java
@@ -0,0 +1,38 @@
+package com.san.newsfeed.member.entity;
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor
+
+
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable=false,unique=true,length=255)
+    private String email;
+
+    @Column(nullable = false,length=255)
+    private String password;
+
+    @Column(nullable = false,length=50)
+    private String username;
+
+    @Column(nullable=false)
+    private LocalDateTime createdAt=LocalDateTime.now();
+
+    @Column(nullable = false)
+    private LocalDateTime updatedAt=LocalDateTime.now();
+
+    @PreUpdate
+    public void onUpdate() {
+    this.updatedAt=LocalDateTime.now();}
+}
+
